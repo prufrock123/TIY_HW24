95597d7c9acdb2ab39d1596071d14a1254984219
prufrock123 <shawnmllr86@gmail.com>
2014-10-23 17:03:53 -0500

    Kicks it up a notch with Foundation's JoyRide JS doo-dad.

--- js/app.js
+++ js/app.js
@@ -12,9 +12,36 @@ function app(){
         {url: "./bower_components/foundation/js/foundation.js"}
     ).then(function(){
         _.templateSettings.interpolate = /{([\s\S]+?)}/g;
-
+        $(document).foundation('joyride', 'start');
         // start app?
+        
+    // var recipe = new leftOver(options);
+
     })
 
 }
+
+// function leftOver(options) {
+//     if (!options.api_key) {
+//         throw new Error("Not going to work w/o API key brah");
+//     };
+// }
+
+
+
+// leftOver.prototype.Routing = function(){
+//     var self = this;
+
+//     Path.map("#/").to(
+//     });
+
+//     Path.map("#/results").to(function() {
+//         $.when(
+
+//         ).then(function () {
+
+//         });
+//     });
+// }
+
 
