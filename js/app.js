f3f4c88ba990dd31a10a0335c4b1a50b3a8325a5
prufrock123 <shawnmllr86@gmail.com>
2014-10-24 14:15:30 -0500

    Cleans up code with the help of jsHint.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ window.onload = app;
 
 // runs when the DOM is loaded
 function app(){
+    "use strict";
 
     // load some scripts (uses promises :D)
     loader.load(
@@ -17,45 +18,48 @@ function app(){
     var options = {
         app_key: "4164fad3825e0f682dfe82b17b4acf89",
         app_id: "2e7123cd"
-    }
+    };
 
-    var recipe = new leftOver(options);
+    var recipe = new LeftOver(options);
 
-    })
+    });
 
 }
 
 // http://api.yummly.com/v1/api/recipes?_app_id=app-id
 // &_app_key=app-key&your _search_parameters
 
-function leftOver(options) {
+function LeftOver(options) {
+    "use strict";
     if (!options.app_key) {
         throw new Error("Not going to work w/o API key brah");
-    };
+    }
     this.yum_url = "http://api.yummly.com/v1/api/recipes?_app_id=";
     this.app_id = options.app_id;    
     this.app_key = options.app_key;
-    this.complete_api_url = this.yum_url + this.app_id 
-        + "&_app_key=" + this.app_key + "&";
+    this.complete_api_url = this.yum_url + this.app_id + "&_app_key=" + this.app_key + "&";
 
     console.log(this.complete_api_url);
     this.Routing();
 }
 
 function joyRide(){ 
+    "use strict";
     $(document).foundation('joyride', 'start');
 }
 
-leftOver.prototype.pullRecipes = function(parameters) {
+LeftOver.prototype.pullRecipes = function(parameters) {
+    "use strict";
     return $.getJSON(
         this.complete_api_url + "&q=onion+soup&allowedIngredient[]=garlic&allowedIngredient[]=cognac")
     .then(function(data){
         // console.log(data);
         return data;
     });
-}
+};
 
-leftOver.prototype.Routing = function(){
+LeftOver.prototype.Routing = function(){
+    "use strict";
     var self = this;
 
     Path.map("#/").to(joyRide);
@@ -65,12 +69,12 @@ leftOver.prototype.Routing = function(){
             self.pullRecipes()
         ).then(function(data) {
             // debugger;
-            console.log(data)
+            console.log(data);
         });
     });
 
     Path.root("#/");
     Path.listen();
-}
+};
 
 
